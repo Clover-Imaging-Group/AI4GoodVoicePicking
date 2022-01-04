fbee91f605a077c13d4140e4045510bf931dcfe6
Min Maung <minmaung@Mins-CNCY-MBP.localdomain>
2022-01-04 10:48:05 -0600

    Adding changes

---
+++ Samples/Web/ConversationAI.DemoSite/ConversationAI.DemoSite/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ConversationAI.DemoSite.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
